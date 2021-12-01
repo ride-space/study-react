fc649d27849ffc5c579b0e5a95e2659bb89f9383
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2021-12-01 13:37:48 +0900

    ♻️ 要素にprevを追加してわかりやすくした。

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,21 +1,36 @@
 import React, { useCallback, useEffect, useState } from "react";
 import Head from "next/head";
 import styles from "src/styles/Home.module.css";
-
 import { Footer } from "src/components/Footer";
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
 
 export default function Home() {
-  const [count,setCount] = useState(1);
-  const [text,setText] = useState("");
-  const [isShow,setIsShow] = useState(true);
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
-  const HandleClick = useCallback((e) => {
-    if(count < 10) {
-      setCount(count => count +1);
+  const HandleClick = useCallback(
+    (e) => {
+      if (count < 10) {
+        setCount((prevCount) => prevCount + 1);
+      }
+    },
+    [count]
+  );
+
+  const HandleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("5文字以内にしてください");
+      return prevArray;
     }
-  },[count]);
+    setText(e.target.value.trim());
+  }, []);
+
+  const HandleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
+  }, []);
 
   const HandleAdd = useCallback(
     function () {
@@ -30,22 +45,13 @@ export default function Home() {
   );
 
   useEffect(() => {
-    console.log('マウント時');
+    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log('アンマウント時');
+      console.log("アンマウント時");
       document.body.style.borderColor = "";
     };
-  },[]);
-
-  const HandleChange = useCallback((e) => {
-    if (e.target.value.length > 5 ) {
-      alert('5文字以内にしてください')
-    }
-    setText(e.target.value.trim());
-  },[]);
-
-  console.log(text);
+  }, []);
 
   return (
     <div className={styles.container}>
@@ -56,8 +62,14 @@ export default function Home() {
       {isShow ? <h1>{count}</h1> : null}
 
       <button onClick={HandleClick}>ボタン</button>
-      <button onClick={HandleDisplay}>{isShow ? "表示":"非表示"}</button>
-      <input type="text" value={text} onChange={HandleChange}/>
+      <button onClick={HandleDisplay}>{isShow ? "表示" : "非表示"}</button>
+      <input type="text" value={text} onChange={HandleChange} />
+      <button onClick={HandleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
