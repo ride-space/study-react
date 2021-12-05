b456a5fd77519b9361872932924067632af8a3ed
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2021-12-05 13:56:23 +0900

    ✨ useRouterをまなびました

---
+++ src/hooks/useBgColor.jsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/dist/client/router";
+import {useEffect, useMemo} from "react";
+
+
+
+export const useBgColor = () => {
+  const router = useRouter();
+  const BgColor = useMemo(() => {
+    return router.pathname === "/" ? "lightblue":"beige";
+  },[router.pathname]);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = BgColor;
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.borderColor = "";
+    };
+  }, [BgColor]);
+};
