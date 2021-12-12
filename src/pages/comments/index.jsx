8a13df0ff9b927a59319023653075060c0184f9b
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2021-12-12 16:18:19 +0900

    ⚡️ commentsのページをSSG化

--- src/pages/comments/index.jsx
+++ src/pages/comments/index.jsx
@@ -1,15 +1,37 @@
 import Head from "next/dist/shared/lib/head";
 import { Header } from "src/components/Header";
 import { Comments as CommentsComponent } from "src/components/Comments";
+import { SWRConfig } from "swr";
 
-const Comments = () => {
+
+export const getStaticProps = async () => {
+  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments`;
+  const comments = await fetch(COMMENTS_API_URL);
+  const commentsData = await comments.json();
+
+  return {
+    props: {
+      fallback: {
+        COMMENTS_API_URL: commentsData,
+      },
+    }
+  };
+};
+
+const Comments = (props) => {
+  const { fallback } = props;
+
   return (
     <div>
       <Head>
         <title>Comments</title>
       </Head>
+      <SWRConfig value={{fallback}}>
+
       <Header />
       <CommentsComponent />
+      </SWRConfig>
+
     </div>
   );
 };
