55b6c308020b34d822ef7383836a008b0e2c1b64
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2021-12-12 12:15:26 +0900

    ✨ userPageをSSR化

--- src/pages/users/[id]/index.jsx
+++ src/pages/users/[id]/index.jsx
@@ -1,13 +1,32 @@
 import { Header } from "src/components/Header";
 import { User } from "src/components/User";
+import { SWRConfig } from "swr";
 
-const UsersId = () => {
+const sleep = (ms) => {new Promise((resolve) => setTimeout(resolve,ms))};
+
+//SSRを行う際、サーバー側で動くコード
+export const getServerSideProps = async (ctx) => {
+  const { id } = ctx.query;
+  const API_URL = `https://jsonplaceholder.typicode.com/users/${id}`;
+  const user = await fetch(API_URL);
+  const userData = await user.json();
+
+  return {
+    props: {
+      fallback: {
+        API_URL: userData,
+      },
+    },
+  };
+};
+
+const UsersId = ({ fallback }) => {
   return (
-    <dir>
+    <SWRConfig value={{ fallback }}>
       <Header />
       <User />
-    </dir>
+    </SWRConfig>
   );
-}
+};
 
 export default UsersId;
