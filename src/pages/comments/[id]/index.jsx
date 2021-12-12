10b676be0660270d20797e8ecf54887caffabb32
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2021-12-12 12:16:12 +0900

    ✨ comments一覧のページをSSG化

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -1,13 +1,34 @@
 import { useRouter } from "next/dist/client/router";
 import { Header } from "src/components/Header";
 import { Comment } from "src/components/Comment";
-const CommentsId = () => {
+import { SWRConfig } from "swr";
+
+//SSGを行う際、サーバー側で動くコード
+export const getStaticProps = async (ctx) => {
+  const { id } = ctx.query;
+  const COMMENTS_API_URL = `https://jsonplaceholder.typicode.com/comments/${id}`;
+  const comments = await fetch(COMMENTS_API_URL);
+  const commentsData = await comments.json();
+
+  return {
+    props: {
+      fallback: {
+        COMMENTS_API_URL: commentsData,
+      },
+    }
+  };
+};
+
+const CommentsId = (props) => {
+  const { fallback } = props;
   const router = useRouter();
   console.log(router);
   return (
     <div>
-      <Header />
-      <Comment />
+      <SWRConfig value={{fallback}}>
+        <Header />
+        <Comment />
+      </SWRConfig>
     </div>
   );
 };
