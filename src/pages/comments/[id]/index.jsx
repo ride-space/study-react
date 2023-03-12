89fe06f1d7137d6cb72ea3076d64f48497788b11
kaazu1224 <75646380+kazun1224@users.noreply.github.com>
2023-03-12 17:20:32 +0900

    SGの実装とISRに変更

--- src/pages/comments/[id]/index.jsx
+++ src/pages/comments/[id]/index.jsx
@@ -4,6 +4,14 @@ import { SWRConfig } from "swr";
 import { API_URL } from "src/utils/const";
 
 //動的なページのSSG
+// SSGのページで動的なページを作成するのに必要な値(id)などを取得してgetStaticPropsに渡す
+
+// getStaticPathsのfallbackで挙動を変化させる
+// fallback: false -> build時に生成されていないページは404を返す。
+// fallback: true -> build時に生成されていないページはユーザが訪れた時に始めて生成されるそれ以降は使いまわし
+// fallback: "blocking" -> build時に生成されていないページはSSRのように事前にbuildして表示(ユーザの画面にこのページに遷移できるリンクが表示されたとき)
+// blockingでユーザの画面に入ったときにbuildされてリクエスト回数が多くいやな場合はこのページのリンク(このアプリでは,CommentList component)で
+// prefetch={false}を指定する(この場合リンクにホバーされたタイミングでbuildで生成されるのでレンダリングは抑えられる)
 export const getStaticPaths = async () => {
   //ctxにわたすためのIDをすべて取得するfetch
   const comments = await fetch(`${API_URL}/comments?_limit=10`);// ?_limit=10でSSG処理を10件に指定
@@ -18,16 +26,25 @@ export const getStaticPaths = async () => {
   };
 };
 
-//SSGを行う際、サーバー側で動くコード
+//SSGを行う際、build時に動くコード
+// build時のみデータを取得して静的ページを作成する
+// 一度build時に生成してあるページを表示するだけなので、フェッチ回数を抑えられる
+// SSRのようにmeta的にもデータが認識されるのでSSRとSSGどちらでもいい場合のみSSGを基本的に使用したほうがいい
+// ページの生成はbuild時にしかされないのでデータの更新はbuildされるまで反映されない
+// リアルタイムで値が変更される要件の場合はあまり向いていない
 export const getStaticProps = async (ctx) => {
   const { id } = ctx.params;
   const COMMENT_API_URL = `${API_URL}/comments/${id}`;
   const comment = await fetch(COMMENT_API_URL);
 
   // 存在しないデータをURIで指定されたときに404を返す処理
+  // getStaticPathsでfallback trueかblockingの場合にデータがないページでも表示しようとするので
+  // データがないページが404ページを表示するように変更
   if (!comment.ok) {
     return {
       notFound: true,
+      // ISR下記とともに追記
+      revalidate: 60,
     };
   }
 
@@ -39,14 +56,26 @@ export const getStaticProps = async (ctx) => {
         [COMMENT_API_URL]: commentData,
       },
     },
+
+    // ISR <-SSGでは更新をbuildされるまで反映されない
+    // ISRでは下記で数字(秒)を指定して指定した時間が過ぎた初めてのリクエスト時に再度buildするようにできる
+    // 上記によって更新も反映できる
     revalidate: 60,
   };
 };
 
+
+
 const CommentsId = (props) => {
   const { fallback } = props;
   const router = useRouter();
-  console.log(router);
+
+
+  // getStaticPathsでfallbackがtrueのときbuild中に表示するコンポーネント(loadingなど)を下記で表示できる
+  // if(router.isFallback) {
+  //   return <div>Loading</div>
+  // }
+
   return (
     <div>
       <SWRConfig value={{ fallback }}>
--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -3,6 +3,13 @@ import { UserList } from "src/components/User/UserList";
 import { API_URL } from "src/utils/const";
 import { SWRConfig } from "swr";
 
+
+// SSR(サーバーサイドレンダリング)
+// サーバー側でリクエストを行い事前にデータを取得する
+// CSRなどでクライアントフェッチではデータが後から呼ばれるためブラウザのmeta dataとしては値が認識されていない
+// SSRは裏でフェッチが行われた後に表示するため、フラウザがデータをmeta的にも認識できる
+// クライアントがこのページに来た時に毎回サーバー側でフェッチする
+// データの読み込みが終わってから画面遷移するのでユーザビリティ的にはあまり良くなくなることもある
 export const getServerSideProps = async () => {
   //user一覧のfetch
   const USERS_API_URL = `${API_URL}/users`;
@@ -18,6 +25,7 @@ export const getServerSideProps = async () => {
   };
 };
 
+// SWRConfigでssgの値をプロップスで渡さなくても扱えるようにしている
 const Users = ({ fallback }) => {
   return (
     <SWRConfig value={{ fallback }}>
